e24b7eb3c67c48f0641378e831d9edcd460a0c58
Simran Singh <siimransiingh08@gmail.com>
2024-03-14 01:47:52 +0530

    cors configured

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ import authRoutes from "./routes/auth";
 import pdfRoutes from "./routes/generatePdf";
 import cookieParser from "cookie-parser";
 import path from "path";
-
+console.log(process.env.FRONTEND_URL)
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
